bddf0567fe483b14e20a6d7c7732eb982b55746e
baolei <1192706763@qq.com>
2022-05-11 11:29:22 +0000

    feat:makeOffer

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 const   Web3 = require('web3')
 const  { OpenSeaPort, Network } = require('opensea-js')
-const axios  = require("axios")
 
 
-const provider = new Web3.providers.HttpProvider(`https://mainnet.infura.io`)
+const provider = new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/49d77d7e91ce4747a8086f4ebbbe98b0`)
 
 const seaport = new OpenSeaPort(provider, {
   networkName: Network.Main,
   apiKey: 'faeb5c87d0ab4d9eb0e84ea026c1b842'
 })
 
+const asset = {
+  tokenAddress:"0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85",
+  tokenId:"108139716447050348910741088656393462735770136017145801888008682844778565891977"
+}
+
 const main  = async ()=>{
     const asset = await seaport.api.getAsset({
     tokenAddress:"0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85", // string
     tokenId:"108139716447050348910741088656393462735770136017145801888008682844778565891977"
     })
-    console.log(asset)
-    // axios.get("https://api.opensea.io/api/v1/asset/0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85/108139716447050348910741088656393462735770136017145801888008682844778565891977/").then(res=>{
-    //     console.log(res.data)
-    // })
+    // console.log(asset);
+    
 }
 
-main()
+const getBalance = async ()=>{
+  const balance = await seaport.getAssetBalance({
+    accountAddress:"0xFA9B690344696a8EC334bebBe269EF2729b40955",
+    asset
+  })
+  console.log(balance.toNumber())
+}
+
+const makeOffer = async ()=>{
+  const offer = await seaport.createBuyOrder({
+    asset:{
+      ...asset,
+    },
+    accountAddress:"0xDc36216742AE7d8a6c80a365713f89fbC74FfE1E",
+    startAmount:0.01,
+  })
+  
+}
+
+// main()
+// getBalance();
+makeOffer()
+
 
