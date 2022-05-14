b22830deea358111b87ee2e7c1c92f6c31211e3e
baolei <1192706763@qq.com>
2022-05-14 13:42:35 +0000

    feat: 添加获取tokenId

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const  { OpenSeaPort, Network } = require('opensea-js');
 const private_key = "";
 const infura_project_id= "b048f1159fef4b2a85839313cff6aed8";
 const wallet_address = "";
-const offer_price = 0.01;
+const offer_price = 0.001;
 const expirationTime = Math.round(Date.now() / 1000 + 60 * 60 * 2) // 过期时间
 
 const provider = new HDWalletProvider({
@@ -19,14 +19,17 @@ const seaport = new OpenSeaPort(provider, {
   apiKey: 'faeb5c87d0ab4d9eb0e84ea026c1b842'
 })
 
-for(let i=0;i<10000;i++){
-  const domain = i+"0000";
-  console.log(`${domain}.eth`);
-  // 传入tokenId 和 price
-  // makeOffer()
+const getTokenId = (name)=>{
+  const ethers = require('ethers');
+  const utils = ethers.utils
+  const lableArr = name.split('.')
+  const labelHash = utils.keccak256(utils.toUtf8Bytes(lableArr[0]))
+  const tokenId = new utils.BigNumber(labelHash).toString()
+  return tokenId;
 }
 
-const makeOffer = (tokenId, price)=>{
+
+const makeOffer =  (tokenId, price)=>{
   seaport.createBuyOrder({
     asset:{
       tokenAddress:"0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85",
@@ -42,5 +45,27 @@ const makeOffer = (tokenId, price)=>{
   })
 }
 
+// const makeBundleOffer = (assets)=>{
+//   seaport.createBundleBuyOrder({
+//     assets,
+//     accountAddress:wallet_address,
+//     startAmount: price,
+//     // Optional expiration time for the order, in Unix time (seconds):
+//     expirationTime
+//   }).then(res=>{
+//     console.log(res)
+//   }).catch(err=>{
+//     console.log(err)
+//   })
+// }
+
+for(let i=0;i<10000;i++){
+  const domain = ("0000"+i).slice(-4);
+  const tokenId = getTokenId(domain);
+  console.log(`${domain}.eth`,`trokenId:${tokenId}`)
+
+  // 传入tokenId 和 price
+  // makeOffer()
+}
 
 
